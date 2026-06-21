1bb8a455c938dc6d04d0d7173ce5bcde203735f6
Damaris <2005.amy.tomala@gmail.com>
2026-06-21 04:20:14 -0500

    Crear vista de registro ciudadano

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,48 +1,50 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sistema de Gestión de Incidencias</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-    
-    @vite(['resources/css/auth.css'])
-</head>
-<body>
+@extends('layouts.guest')
 
-    <div class="container login-container">
+@section('content')
+
+<div class="container login-container">
     <div class="card card-login mb-3 shadow-lg">
-        <div class="row g-0 ">
+        <div class="row g-0">
             <div class="col-md-5 p-0 d-flex">
-            <img clas src="{{ asset('img/login/login-banner.png') }}" class="img-fluid rounded-start" alt="..." >
+                <img src="{{ asset('img/login/login-banner.png') }}"
+                     class="img-fluid rounded-start"
+                     alt="Login">
             </div>
             <div class="col-md-7">
-            <div class="card-body login-body">
-                <div class="text-center mb-4">
-                <h1> Sistema de Gestión de Incidencias Georreferenciadas</h1>
-                <p class="text-muted">Ingrese sus credenciales para acceder</p>
-            </div>
-            <form>
-                <div class="mb-3">
-                    <label class="form-label">Correo electrónico</label>
-                    <input type="email" class="form-control">
+                <div class="card-body login-body">
+                    <div class="text-center mb-4">
+                        <h1>Sistema de Gestión de Incidencias Georreferenciadas</h1>
+                        <p class="text-muted">
+                            Ingrese sus credenciales para acceder
+                        </p>
+                    </div>
+                    <form>
+                        <div class="mb-3">
+                            <label class="form-label">
+                                Correo electrónico
+                            </label>
+                            <input type="email" class="form-control">
+                        </div>
+                        <div class="mb-4">
+                            <label class="form-label">
+                                Contraseña
+                            </label>
+                            <input type="password" class="form-control">
+                        </div>
+                        <button class="btn btn-primary w-100">
+                            Iniciar sesión
+                        </button>
+                        <div class="text-center mt-3">
+                            ¿No tienes una cuenta?
+                            <a href="{{ route('register') }}">
+                                Registrarse
+                            </a>
+                        </div>
+                    </form>
                 </div>
-                <div class="mb-4">
-                    <label class="form-label">Contraseña</label>
-                    <input type="password" class="form-control">   
-                </div>
-                <button class="btn btn-primary w-100">Iniciar sesión</button>
-                <div class="text-center mt-3">
-                    ¿No tienes una cuenta?
-                    <a href="{{ route('register') }}">Registrarse</a>
-                </div>
-            </form>
-            </div>
             </div>
         </div>
     </div>
+</div>
 
-
-
-</body>
-</html>
+@endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,35 +1,71 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registrarse</title>
-</head>
-<body>
-    <div class="container register-container">
-        <h1>Registro de Usuario</h1>
-        <form method="POST" action="{{ route('register') }}">
-            @csrf
-            <div>
-                <label for="name">Nombre:</label>
-                <input type="text" id="name" name="name" required>
+@extends('layouts.guest')
+
+@section('content')
+
+<div class="container register-container">
+    <div class="card card-register mb-3 shadow-lg">
+    <img class="img-register" src="{{ asset('img/login/register.png') }}" class="card-img-top" alt="Registro ciudadano">
+    <div class="card-body register-body">
+        <h4 class="card-title text-center">Registro ciudadano</h4>
+        <p class="card-text text-center">Crea tu cuenta para reportar incidencias.</p>
+        <form class="row g-3">
+            <div class="col-md-6">
+                <label class="form-label">Nombres</label>
+                <input type="text" class="form-control">
             </div>
-            <div>
-                <label for="email">Correo Electrónico:</label>
-                <input type="email" id="email" name="email" required>
+            <div class="col-md-6">
+                <label class="form-label">Apellidos</label>
+                <input type="text" class="form-control">
             </div>
-            <div>
-                <label for="password">Contraseña:</label>
-                <input type="password" id="password" name="password" required>
+            <div class="col-md-6">
+                <label for="inputEmail4" class="form-label">Email</label>
+                <input type="email" class="form-control" id="inputEmail4">
             </div>
-            <div>
-                <label for="password_confirmation">Confirmar Contraseña:</label>
-                <input type="password" id="password_confirmation" name="password_confirmation" required>
+            <div class="col-md-6">
+                <label for="inputPhone" class="form-label">Teléfono</label>
+                <input type="text" class="form-control" id="inputPhone">
             </div>
-            <button type="submit">Registrarse</button>
+            <div class="col-md-6">
+                <label for="inputState" class="form-label">País</label>
+                <select id="inputState" class="form-select">
+                <option selected>Choose...</option>
+                <option>...</option>
+                </select>
+            </div>
+            <div class="col-md-6">
+                <label for="inputState" class="form-label">Provincia</label>
+                <select id="inputState" class="form-select">
+                <option selected>Choose...</option>
+                <option>...</option>
+                </select>
+            </div>
+            <div class="col-md-4">
+                <label for="inputState" class="form-label">Ciudad</label>
+                <select id="inputState" class="form-select">
+                <option selected>Choose...</option>
+                <option>...</option>
+                </select>
+            </div>
+            <div class="col-md-8">
+                <label for="inputAddress" class="form-label">Dirección</label>
+                <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+            </div>
+            <div class="col-md-6">
+                <label for="inputPassword4" class="form-label">Contraseña</label>
+                <input type="password" class="form-control" id="inputPassword4">
+            </div>
+            <div class="col-md-6">
+                <label for="inputPassword4" class="form-label">Confirmar contraseña</label>
+                <input type="password" class="form-control" id="inputPassword4">   
+            </div>
+                <button type="submit" class="btn btn-primary">Registrar</button>
+            <div class="text-center mt-3">
+                ¿Ya tienes una cuenta?
+                <a href="{{ route('login') }}">Iniciar sesión</a>
+            </div>
         </form>
-        <p>¿Ya tienes una cuenta? <a href="{{ route('login') }}">Iniciar sesión</a></p>
     </div>
-    
-</body>
-</html>
+    </div>
+</div>
+
+@endsection
